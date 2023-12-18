7a7b7488859f88ed14c2f3f80849f53741b68a83
Virendra Kumar Paswan <vp222370@gmail.com>
2023-12-19 04:46:05 +0530

    footer

---
+++ ecommerce/src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainCrosel from '../../components/HomeCarosel/MainCrosel'
+
+const HomePage = () => {
+    return (
+        <div>
+            <MainCrosel/>
+        <div>
+            Other section
+        </div>
+        </div>
+
+    )
+}
+
+export default HomePage
